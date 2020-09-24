1b58754a396d2b38c0b08511bd1fd1c54549301a
Dylan Weyers <59836383+dylanweyers@users.noreply.github.com>
2020-09-24 15:14:09 +0100

    Update lweps_cfg.lua

--- lweps/lua/lweps/lweps_cfg.lua
+++ lweps/lua/lweps/lweps_cfg.lua
@@ -1,16 +1,18 @@
+// Config file
 lwep = {}
 
-lwep.npcmodel = "models/dog.mdl"
-
+// Strings to be recognized as commands that will open the menu
 lwep.cmds = {
 	["!permweps"] = true,
 	["!lweps"] = true,
 }
 
+// Table of specific users that can access the menu regardless of their in-game rank
 lwep.users = {
 	["STEAM_0:1:64763283"] = true,
 }
 
+// Table of in-game ranks that can open the menu
 lwep.ranks = {
 	["owner"] = true,
 	["co-owner"] = true,
